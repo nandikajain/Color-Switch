ab1f81fbf1125fbe727de2414be80dcd6e347885
arora-ansh <ansh19022@iiitd.ac.in>
2020-11-21 22:17:49 +0530

    Design changes GUI

--- src/MainMenuController.java
+++ src/MainMenuController.java
@@ -1,3 +1,20 @@
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
 public class MainMenuController {
 
+    @FXML
+    public void onExitClicked(){
+        System.exit(1);
+    }
+
+    @FXML
+    public void onSavedGamesClicked(MouseEvent mouseEvent) {
+
+    }
+
+    @FXML
+    public void onPlayClicked(MouseEvent mouseEvent) {
+
+    }
 }
